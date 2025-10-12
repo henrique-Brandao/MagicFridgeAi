2d5b0cb6993bb9145149579a1d365cb3d0b90c2f
henrique-brandao <henriquefr321@gmail.com>
2025-10-12 13:53:56 -0300

    feat: Adicionando um metodo para poder converter o enum minusculo em maisculo

--- src/main/java/com/example/MagicFridgeAI/enums/Categoria.java
+++ src/main/java/com/example/MagicFridgeAI/enums/Categoria.java
@@ -1,5 +1,7 @@
 package com.example.MagicFridgeAI.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum Categoria {
     VEGETAL,
     FRUTA,
@@ -8,5 +10,10 @@ public enum Categoria {
     GORDURA,
     DOCE,
     BEBIDA,
-    GRAO
+    GRAO;
+
+    @JsonCreator
+    public static Categoria fromString(String value) {
+        return Categoria.valueOf(value.toUpperCase());
+    }
 }
